5c6845fc0c6e243510a540c1180dedd78233c6a3
Dmytro Rudenko <rud.rud.1999@gmail.com>
2019-07-14 18:35:43 +0300

    Create main.cpp

---
+++ Course #1 (White Belt)/Week 3/04. Names and Surnames I/main.cpp
@@ -0,0 +1,46 @@
+#include <map>
+#include <string>
+
+using namespace std;
+
+string FindNameByYear(const map<int, string>& names, int year) {
+    string name;
+    
+    for (const auto& item : names) {
+        if (item.first <= year) {
+            name = item.second;
+        } else {
+            break;
+        }
+    }
+
+    return name;
+}
+
+class Person {
+public:
+    void ChangeFirstName(int year, const string& first_name) {
+        first_names[year] = first_name;
+    }
+    void ChangeLastName(int year, const string& last_name) {
+        last_names[year] = last_name;
+    }
+    string GetFullName(int year) {
+        const string first_name = FindNameByYear(first_names, year);
+        const string last_name = FindNameByYear(last_names, year);
+
+        if (first_name.empty() && last_name.empty()) {
+            return "Incognito";
+        } else if (first_name.empty()) {
+            return last_name + " with unknown first name";
+        } else if (last_name.empty()) {
+            return first_name + " with unknown last name";
+        } else {
+            return first_name + " " + last_name;
+        }
+    }
+
+private:
+    map<int, string> first_names;
+    map<int, string> last_names;
+};
