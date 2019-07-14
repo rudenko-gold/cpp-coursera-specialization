f1f304266f7a79152d4a92594644f91a9ea652d2
Dmytro Rudenko <rud.rud.1999@gmail.com>
2019-07-14 16:06:11 +0300

    Create main.cpp

---
+++ Course #1 (White Belt)/Week 3/03. Sorted Strings/main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class SortedStrings {
+public:
+    void AddString(const string& s) {
+        data.push_back(s);
+    }
+    vector<string> GetSortedStrings() {
+        sort(begin(data), end(data));
+        return data;
+    }
+private:
+    vector<string> data;
+};
