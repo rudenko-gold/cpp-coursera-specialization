4f101990c0892b4da31fe12efa443847e978baaf
Dmytro Rudenko <rud.rud.1999@gmail.com>
2019-07-17 14:43:18 +0300

    Update main.cpp

--- Course #3 (Red Belt)/Week 1/01. Macros LOG/main.cpp
+++ Course #3 (Red Belt)/Week 1/01. Macros LOG/main.cpp
@@ -4,39 +4,39 @@ using namespace std;
 
 class Logger {
 public:
-  explicit Logger(ostream& output_stream) : os(output_stream) {
-  }
+    explicit Logger(ostream& output_stream) : os(output_stream) {
+    }
 
-  void SetLogLine(bool value) { log_line = value; }
-  void SetLogFile(bool value) { log_file= value; }
+    void SetLogLine(bool value) { log_line = value; }
+    void SetLogFile(bool value) { log_file= value; }
 
-  void Log(const string& message);
+    void Log(const string& message);
 
-  void SetFile(const string& filename) {
-    file = filename;
-  }
+    void SetFile(const string& filename) {
+        file = filename;
+    }
 
-  void SetLine(int line_number) {
-    line = line_number;
-  }
+    void SetLine(int line_number) {
+        line = line_number;
+    }
 
 private:
-  ostream& os;
-  bool log_line = false;
-  bool log_file = false;
-  string file;
-  int line;
+    ostream& os;
+    bool log_line = false;
+    bool log_file = false;
+    string file;
+    int line;
 };
 
 void Logger::Log(const string& message) {
-  if (log_file && log_line) {
-    os << file << ':' << line << ' ';
-  } else if (log_file) {
-    os << file << ' ';
-  } else if (log_line) {
-    os << "Line " << line << ' ';
-  }
-  os << message << '\n';
+    if (log_file && log_line) {
+        os << file << ':' << line << ' ';
+    } else if (log_file) {
+        os << file << ' ';
+    } else if (log_line) {
+        os << "Line " << line << ' ';
+    }
+    os << message << '\n';
 }
 
 #define LOG(logger, message) \
