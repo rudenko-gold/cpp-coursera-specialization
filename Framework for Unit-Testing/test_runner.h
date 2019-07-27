9c288e2966419d209aafa0d0111a6cb4d4502788
Dmytro Rudenko <rud.rud.1999@gmail.com>
2019-07-27 11:03:30 +0300

    Update test_runner.h

--- Framework for Unit-Testing/test_runner.h
+++ Framework for Unit-Testing/test_runner.h
@@ -39,6 +39,20 @@ ostream& operator << (ostream& os, const set<T>& s) {
 }
 
 template <class K, class V>
+ostream& operator << (ostream& os, const unordered_map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
+}
+
+template <class K, class V>
 ostream& operator << (ostream& os, const map<K, V>& m) {
     os << "{";
     bool first = true;
