265805a0fc9daf0398f9d93fb1162912e30cc96b
Dmytro Rudenko <rud.rud.1999@gmail.com>
2020-03-03 16:06:15 +0200

    Create bus_database.h

---
+++ Course #4 (Brown Belt)/Week 6/Transport Guide/A/bus_database.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+#include "string_util.h"
+
+class Route {
+public:
+    std::string title;
+    std::vector<std::string> stops;
+};
+
+bool operator== (const Route& lhs, const Route& rhs);
+std::ostream& operator<< (std::ostream& os, const Route& route);
+
+Route readRouteInfo(std::istream& is);
+
+class RouteDatabase {
+public:
+    void readDatabase(std::istream& is);
+    void addRoute(Route route);
+    const Route& getRoute(const std::string& title);
+private:
+    std::unordered_map<std::string, Route> route_;
+    friend bool operator== (const RouteDatabase& lhs, const RouteDatabase& rhs);
+};
